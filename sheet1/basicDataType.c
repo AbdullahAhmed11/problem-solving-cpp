42e441448fe3d224b1fa0bbd05c8a65ad68188ba
AbdullahAhmed11 <abdullah77zzx@gmail.com>
2023-02-18 17:04:54 +0200

    firstUpdate

---
+++ sheet1/basicDataType.c
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    long long b;
+    string c;
+    double d, f;
+    cin >> a >> b >> c >> d >> f;
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    cout << d << "\n";
+    cout << f;
+    return 0;
+}
